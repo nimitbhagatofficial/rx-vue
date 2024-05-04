11c4b33f4d2324d01a43c1d63b11b3e414bb720e
Nimit Bhagat <nimit.bhagat@radixweb.com>
2024-05-04 17:54:28 +0530

    Exam

---
+++ src/components/StepThree.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="col-md-6">
+        <h1>Summary</h1>
+        <table>
+            <tr>
+                <td>{{ selectedConfig }}</td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["selectedModal", "selectedColor", "selectedConfig"],
+    data() {
+        return {}
+    },
+    computed: {},
+    methods: {},
+}
+</script>
---
+++ src/components/StepTwo.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="col-md-6">
+
+        <div class="form-group">
+            <label for=""></label>
+            <select class="form-control" v-model="config" @change="settingsChanged">
+                <option v-for="(config, index) in configurations.configs" :value="index">
+                    {{ config.description }} - ${{ config.price }}
+                </option>
+            </select>
+        </div>
+
+        <ul>
+            <li>Color - {{ selectedColor.description }} - ${{ selectedColor.price }}</li>
+            <li>
+                Range/Speed - {{ selectedConfig.range }}/{{ selectedConfig.speed }} - ${{ selectedConfig.price }}
+            </li>
+            <li>Tow Hitch - {{ configurations.towHitch ? "YES" : "No" }}</li>
+            <li>Yoke - {{ configurations.yoke ? "YES" : "No" }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    props: ["selectedModal", "selectedColor"],
+    data() {
+        return {
+            configurations: false,
+            config: "",
+            selectedConfig: {},
+        }
+    },
+    computed: {},
+    methods: {
+        getConfigurations() {
+            try {
+                const response = axios.get('/modal/' + this.selectedModal.code, {
+                    headers: {
+                        Accept: "application/json"
+                    },
+                }).then((result) => {
+                    this.config = 0;
+                    this.configurations = result.data;
+                    this.selectedConfig = this.configurations.configs[0];
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        },
+        configChange() {
+            this.$emit('config-changed', this.configurations, this.selectedConfig);
+        },
+        settingsChanged() {
+            this.selectedConfig = this.configurations.configs[this.config];
+        }
+    },
+}
+</script>
